3d4b350d9cafbe4f59619551887e5632789d9613
justins83 <justins83.waze@gmail.com>
2019-04-22 11:41:19 -0400

    Create ZoomShortcuts.js

---
+++ ZoomShortcuts.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name             WME Zoom Shortcuts
+// @namespace        https://greasyfork.org/en/users/166843-wazedev
+// @version          0.1
+// @description      Adds configurable shortcuts for all zoom levels
+// @author           JustinS83
+// @include          https://www.waze.com/editor*
+// @include          https://www.waze.com/*/editor*
+// @include          https://beta.waze.com/editor*
+// @include          https://beta.waze.com/*/editor*
+// @exclude          https://www.waze.com/*user/editor*
+// @grant            none
+// @require          https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
+// @contributionURL  https://github.com/WazeDev/Thank-The-Authors
+// ==/UserScript==
